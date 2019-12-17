d12a33f97b17834f9171fa461f5f1710c737d1ea
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-17 02:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectWithKeyAndValue(object, key, value){
-  var object = {[key] : ' new value' };
+  var object = {[key] : 'new value' };
   return Object.assign(object, {prop2 : 2 });
 }
 
