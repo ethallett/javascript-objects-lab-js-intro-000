a8f26d8f9e31dc047fb1af4b3c38786d4b32b4ab
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-17 02:26:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updateObjectWithKeyAndValue(object, key, value){
   var object = {[key] : 'value' };
-  return Object.assign(object, {[key] : 'value'});
+  return Object.assign(object, {[key] : 'new value' });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
