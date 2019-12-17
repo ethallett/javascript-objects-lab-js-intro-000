a08ad210220300d630cf8c81e79fd3a0c916d5b1
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-17 01:39:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function updateObjectWithKeyAndValue(object, key, value){
+ var object = {prop: 1};
+ var newObject = Object.assign({prop2: 2}, object);
+ return newObject;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object.prop2 = 2
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObject = Object.assign({}, object)
+  return delete newObject.object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+   return delete object.prop
+
+}
